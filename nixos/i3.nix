a922416c24918457a0a11f7e3594f13b04414176
Carlos Miguel Alonso <1998cma@gmail.com>
2023-04-16 20:23:50 +0200

    task: New login (mini-lightdm-greeter)

--- nixos/i3.nix
+++ nixos/i3.nix
@@ -6,6 +6,36 @@
 
     enable = true;
 
+    displayManager.lightdm.greeters.mini = {
+      enable = true;
+      user = "carlos";
+      extraConfig = ''
+      [greeter]
+      show-password-label = false
+      show-input-cursor = false
+      password-alignment = center
+
+      [greeter-hotkeys]
+      mod-key = meta
+      shutdown-key = p
+      restart-key = r
+      hibernate-key = h
+      suspend-key = s
+
+      [greeter-theme]
+      background-image = ""
+      background-color = "#000000"
+      window-color = "#585858"
+      border-color = "#585858"
+      border-width = 1px
+      layout-space = 5
+      password-color = "#FFFFFF"
+      password-background-color = "#000000"
+      password-border-color = "#000000"
+      password-border-width = 1px
+      '';
+    };
+
     desktopManager = {
       xterm.enable = false;
       wallpaper = {
@@ -24,15 +54,15 @@
       extraPackages = with pkgs; [
         i3
         i3lock
-	i3lock-color
-	i3status
+        i3lock-color
+        i3status
         polybar
         dmenu
         rofi
         lxappearance
       ];
 
-      # Default configFile = "$HOME/.i3/config" 
+      # Default configFile = "$HOME/.i3/config"
       configFile = "/home/carlos/.dotfiles/i3/config";
     };
   };
